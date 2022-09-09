7a34112eae98ec73a4a63deb36c3139b68eafd6c
Marko Skender <marko.skender@leaseplan.com>
2022-09-09 09:19:07 +0200

    [patch] Add VPC ID output

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,16 @@
 
 output private_subnets {
-    
+
     value = var.create_private_subnets ? aws_subnet.private : [ ]
 
 }
 
 output public_subnets {
-    
+
     value = var.create_public_subnets ? aws_subnet.public : []
 
+}
+
+output vpc_id {
+    value = aws_vpc.main.id
 }
